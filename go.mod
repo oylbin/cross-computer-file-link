857ba7ae07c6d3a4edfcdb0f51ee0e579889ad46
oylbin <oylbin@gmail.com>
2024-10-17 15:54:19 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/oylbin/cross-computer-file-link
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/redirect/", redirectHandler)
+	http.ListenAndServe(":10114", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		http.Error(w, "Unable to determine home directory", http.StatusInternalServerError)
+		return
+	}
+
+	if runtime.GOOS == "windows" {
+		homeDir = strings.ReplaceAll(homeDir, "\\", "\\\\")
+	}
+	// Serve a simple HTML page with a form and JavaScript for URL conversion and clipboard copying
+	fmt.Fprintf(w, `
+		<html>
+		<body>
+			<input type="text" id="path" placeholder="Enter local file path" />
+			<button onclick="convertAndCopy()">Convert and Copy</button>
+			<script>
+				function convertAndCopy() {
+					var localPath = document.getElementById('path').value;
+					var homeDir = "%s";
+					var relativePath = localPath.replace(homeDir, '').replace(/\\/g, '/');
+					var url = "http://localhost:10114/redirect" + relativePath;
+					navigator.clipboard.writeText(url).then(function() {
+						alert('URL copied to clipboard: ' + url);
+					}, function(err) {
+						console.error('Could not copy text: ', err);
+					});
+				}
+			</script>
+		</body>
+		</html>
+	`, homeDir)
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	homeDir, _ := os.UserHomeDir()
+	relativePath := strings.TrimPrefix(r.URL.Path, "/redirect/")
+	if runtime.GOOS == "windows" {
+		relativePath = strings.ReplaceAll(relativePath, "/", "\\")
+	}
+	localPath := filepath.Join(homeDir, relativePath)
+	openFile(localPath)
+	fmt.Fprintf(w, `
+		<html>
+		<body>
+			<p>Opening file: %s</p>
+			<p>This window cannot be automatically closed. You may close it manually.</p>
+			<script>
+				setTimeout(function() {
+					if (window.opener) {
+						window.close();
+					} else {
+						alert("This window cannot be automatically closed. You may close it manually.");
+					}
+				}, 5000);
+			</script>
+		</body>
+		</html>
+	`, localPath)
+}
+
+func openFile(path string) {
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", path)
+	case "darwin":
+		cmd = exec.Command("open", path)
+	case "linux":
+		cmd = exec.Command("xdg-open", path)
+	}
+	cmd.Start()
+}
